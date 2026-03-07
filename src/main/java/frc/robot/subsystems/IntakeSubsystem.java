44fb66865b380123bbac1e37530bb47ab49d4e03
foxjf92 <101567650+foxjf92@users.noreply.github.com>
2026-03-07 09:31:09 -0500

    Subsystem setup

---
+++ src/main/java/frc/robot/subsystems/IntakeSubsystem.java
@@ -0,0 +1,70 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.subsystems;
+
+import com.revrobotics.spark.SparkFlex;
+import com.revrobotics.spark.config.SparkFlexConfig;
+
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class IntakeSubsystem extends SubsystemBase {
+  /** Creates a new ExampleSubsystem. */
+
+  private SparkFlex intakeLeftVortex; 
+  private SparkFlex intakeRightVortex; 
+  private SparkFlexConfig intakeVortexConfig;
+
+  public IntakeSubsystem() {
+
+    intakeVortexConfig = new SparkFlexConfig();
+        
+        intakeVortexConfig
+          .smartCurrentLimit(80)
+          .idleMode(com.revrobotics.spark.config.SparkBaseConfig.IdleMode.kBrake)
+          .voltageCompensation(12);
+
+        intakeLeftVortex = new SparkFlex(17, com.revrobotics.spark.SparkLowLevel.MotorType.kBrushless);
+        intakeLeftVortex.configure(intakeVortexConfig, com.revrobotics.spark.SparkBase.ResetMode.kResetSafeParameters, com.revrobotics.spark.SparkBase.PersistMode.kNoPersistParameters);
+        
+        intakeRightVortex = new SparkFlex(18, com.revrobotics.spark.SparkLowLevel.MotorType.kBrushless);
+        intakeRightVortex.configure(intakeVortexConfig, com.revrobotics.spark.SparkBase.ResetMode.kResetSafeParameters, com.revrobotics.spark.SparkBase.PersistMode.kNoPersistParameters);
+
+  }
+
+  /**
+   * Example command factory method.
+   *
+   * @return a command
+   */
+  public Command exampleMethodCommand() {
+    // Inline construction of command goes here.
+    // Subsystem::RunOnce implicitly requires `this` subsystem.
+    return runOnce(
+        () -> {
+          /* one-time action goes here */
+        });
+  }
+
+  /**
+   * An example method querying a boolean state of the subsystem (for example, a digital sensor).
+   *
+   * @return value of some boolean subsystem state, such as a digital sensor.
+   */
+  public boolean exampleCondition() {
+    // Query some boolean state, such as a digital sensor.
+    return false;
+  }
+
+  @Override
+  public void periodic() {
+    // This method will be called once per scheduler run
+  }
+
+  @Override
+  public void simulationPeriodic() {
+    // This method will be called once per scheduler run during simulation
+  }
+}
---
+++ src/main/java/frc/robot/subsystems/LauncherSubsystem.java
@@ -0,0 +1,79 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.subsystems;
+
+import com.revrobotics.spark.SparkBase.PersistMode;
+import com.revrobotics.spark.SparkBase.ResetMode;
+import com.revrobotics.spark.SparkFlex;
+import com.revrobotics.spark.SparkLowLevel.MotorType;
+import com.revrobotics.spark.config.SparkBaseConfig.IdleMode;
+import com.revrobotics.spark.config.SparkFlexConfig;
+
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class LauncherSubsystem extends SubsystemBase {
+  /** Creates a new ExampleSubsystem. */
+  
+  private SparkFlex launcherLeftVortex; 
+  private SparkFlex launcherRightVortex; 
+  private SparkFlexConfig launcherVortexConfig;
+  
+  public LauncherSubsystem() {
+
+    launcherVortexConfig = new SparkFlexConfig();
+        
+        launcherVortexConfig
+          .smartCurrentLimit(80)
+          .idleMode(IdleMode.kBrake)
+          .voltageCompensation(12);
+
+        launcherLeftVortex = new SparkFlex(15, MotorType.kBrushless);
+        launcherLeftVortex.configure(launcherVortexConfig, ResetMode.kResetSafeParameters, PersistMode.kNoPersistParameters);
+        
+        launcherRightVortex = new SparkFlex(16, MotorType.kBrushless);
+        launcherRightVortex.configure(launcherVortexConfig, ResetMode.kResetSafeParameters, PersistMode.kNoPersistParameters);
+
+  }
+
+  /**
+   * Example command factory method.
+   *
+   * @return a command
+   */
+  public Command exampleMethodCommand() {
+    // Inline construction of command goes here.
+    // Subsystem::RunOnce implicitly requires `this` subsystem.
+    return runOnce(
+        () -> {
+          /* one-time action goes here */
+        });
+  }
+
+  /**
+   * An example method querying a boolean state of the subsystem (for example, a digital sensor).
+   *
+   * @return value of some boolean subsystem state, such as a digital sensor.
+   */
+  public boolean exampleCondition() {
+    // Query some boolean state, such as a digital sensor.
+    return false;
+  }
+
+  public boolean atSpeed() {
+    // Query some boolean state, such as a digital sensor.
+    return false;
+  }
+
+  @Override
+  public void periodic() {
+    // This method will be called once per scheduler run
+  }
+
+  @Override
+  public void simulationPeriodic() {
+    // This method will be called once per scheduler run during simulation
+  }
+}
